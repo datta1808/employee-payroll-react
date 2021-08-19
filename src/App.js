70d181402be3528d364e6537b7aa990a244dda98
datta1808 <bagaledatta18@gmail.com>
2021-08-20 01:57:54 +0530

    feat : created user registration page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Login from './components/Login'
+import Signup from './components/Signup'
 
 function App() {
   return (
-    <Login />
+    <Login />,
+    <Signup />
   );
 }
 
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,8 +1,14 @@
 import React from 'react'
-import { Grid, Paper, Avatar, Typography } from '@material-ui/core'
+import { Grid, Paper, Avatar, Typography, TextField, Button } from '@material-ui/core'
 import AddCircleOutlinedIcon from '@material-ui/icons/AddCircleOutlined'
 
 const Signup = () => {
+
+    const paperStyle = {padding:'30px 20px', width:500, margin:"150px auto"}
+    const headerStyle = {margin: 0}
+    const avatarStyle = {backgroundColor: '#1bbd7e'}
+    const btnStyle = {margin: '20px 0'}
+
     return (
         <Grid>
             <Paper elevation={20} style={paperStyle}>
@@ -10,10 +16,21 @@ const Signup = () => {
                     <Avatar style={avatarStyle}>
                         <AddCircleOutlinedIcon />
                     </Avatar>
+                    <h2 style={headerStyle}>Registration Form</h2>
+                    <Typography variant='caption'>Please fill this from to create an account!</Typography>
                 </Grid>
+
+                <form>
+                    <TextField fullWidth label='First Name'/>
+                    <TextField fullWidth label='Last Name'/>
+                    <TextField fullWidth label='Email'/>
+                    <TextField fullWidth label='Password'/>
+                    <TextField fullWidth label='Confirm Password'/>
+                    <Button type='submit' variant='contained' color='primary' style={btnStyle}>SIGN UP</Button>
+                </form>
             </Paper>
         </Grid>
     )
 }
 
-export default signup
+export default Signup;
