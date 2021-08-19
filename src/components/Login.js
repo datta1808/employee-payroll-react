2caa68fd3a5a9359df02187c3cb371e3353b1e83
datta1808 <bagaledatta18@gmail.com>
2021-08-20 01:38:39 +0530

    feat : created user login page

---
+++ src/components/Login.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Grid, Paper, Avatar, TextField, Typography, Link, Button } from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+
+const Login = () => {
+
+    const paperStyle = {padding:20, height:'70vh', width:450, margin:'100px auto'}
+    const avatarStyle = {backgroundColor: '#1bbd7e'}
+    const btnstyle = {margin: '16px 0'}
+
+    return (
+        <Grid>
+            <Paper elevation={10} style={paperStyle}>
+                <Grid align='center'>
+                    <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
+                    <h2>Sign In</h2>
+                </Grid>
+                <TextField label='Email' fullWidth required/>
+                <TextField label='Password' type='password' fullWidth required/>
+
+                <Button type='submit' color='primary' variant='contained' style={btnstyle} fullWidth>Sign In</Button>
+                
+                <Typography>
+                    New User?
+                    <Link href="#">
+                        Create Account
+                    </Link>
+                </Typography>
+            </Paper>
+        </Grid>
+    )
+}
+
+export default Login
